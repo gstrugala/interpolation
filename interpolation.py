ca3afa78b99bc5f105f98f3e24576a2417f9a33c
Gregor Strugala <gregor.strugala@gmail.com>
2019-10-11 16:26:15 -0400

    Make the interpolation

--- interpolation.py
+++ interpolation.py
@@ -31,9 +31,18 @@ with open('permap.txt', 'r') as f:
     for i in range(len(nf_products)-2, -1, -1):
         nf_products[i] *= nf_products[i+1]
     C = (np.array(i) for i in itertools.product([0,1], repeat=len(nf_products)))
-    for _ in range(2**len(nf_products)):
-        print(np.dot(nf_products, locs + next(C)))
-    print(nf_products)
-    print(features)
-    print(nfeatures)
-    print(locs)
+    lines = np.array([np.dot(nf_products, locs + next(C)) for i in range(2**len(nf_products))])
+    performance_data = np.zeros((2**len(nf_products), 3))
+    for _ in range(lines[0]):
+        f.readline()
+    for i, nlines in enumerate(np.insert(np.diff(lines), 0, 1)):
+        for _ in range(nlines - 1):
+            f.readline()
+        performance_data[i, :] = np.array(f.readline().split()[5:]).astype('float')
+
+weights = []
+for feature, loc, target_coord in zip(features, locs, target):
+    weights.append((feature[loc+1] - target_coord) / (feature[loc+1] - feature[loc]))
+for weight in weights:
+    performance_data = lower_values, upper_values = np.split(performance_data, 2, axis=0)
+    performance_data = weight * lower_values + (1-weight) * upper_values
