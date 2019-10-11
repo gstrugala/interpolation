4b4ea9cbdabf41b17e823292981cccff9143717b
Gregor Strugala <gregor.strugala@gmail.com>
2019-10-11 14:04:07 -0400

    Compute the lines used for the interpolation

--- interpolation.py
+++ interpolation.py
@@ -1,4 +1,7 @@
+import numpy as np
 from math import floor
+import itertools
+
 def find(array, value):
     L = 0
     R = len(array)
@@ -15,7 +18,6 @@ def num(s):
         return int(s)
     except ValueError:
         return float(s)
-
 target = (22, 0.7, 35, 0.6, 0.4)
 
 with open('permap.txt', 'r') as f:
@@ -24,7 +26,14 @@ with open('permap.txt', 'r') as f:
         features.append([num(v) for v in f.readline().split(' : ')[1].split(', ')])
     f.readline()
     nfeatures = [int(float(n)) for n in f.readline().split()]
-    locs = [find(feature, value) for feature, value in zip(features, target)]
+    locs = np.array([find(feature, value) for feature, value in zip(features, target)])
+    nf_products = np.append(nfeatures[1:], 1)
+    for i in range(len(nf_products)-2, -1, -1):
+        nf_products[i] *= nf_products[i+1]
+    C = (np.array(i) for i in itertools.product([0,1], repeat=len(nf_products)))
+    for _ in range(2**len(nf_products)):
+        print(np.dot(nf_products, locs + next(C)))
+    print(nf_products)
     print(features)
     print(nfeatures)
     print(locs)
