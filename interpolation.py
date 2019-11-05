e13a4d92834e4b59fc3b32209c9bebc887aabe37
Gregor Strugala <gregor.strugala@gmail.com>
2019-11-05 15:29:42 -0500

    Add triangular interpolation

--- interpolation.py
+++ interpolation.py
@@ -20,48 +20,111 @@ def num(s):
         return float(s)
 target = (22, 0.7, 35, 0.6, 0.4)
 
-with open('permap.txt', 'r') as f:
-    features = []
-    nfeatures = []
-    for _ in range(5):
-        s = f.readline().replace(',', '').split(' ')
-        nfeatures.append(num(s[2]))
-        features.append([num(v) for v in s[4:]])
-    f.readline()
-    locs = np.array([find(feature, value) for feature, value in zip(features, target)])
-    nf_products = np.append(nfeatures[1:], 1)
-    for i in range(len(nf_products)-2, -1, -1):
-        nf_products[i] *= nf_products[i+1]
-    C = (np.array(i) for i in itertools.product([0,1], repeat=len(nf_products)))
-    lines = np.array([np.dot(nf_products, locs + next(C)) for i in range(2**len(nf_products))])
-    performance_data = np.zeros((2**len(nf_products), 3))
-    for _ in range(lines[0]):
+def F_M(target):
+    with open('permap.txt', 'r') as f:
+        features = []
+        nfeatures = []
+        for _ in range(5):
+            s = f.readline().replace(',', '').split(' ')
+            nfeatures.append(num(s[2]))
+            features.append([num(v) for v in s[4:]])
         f.readline()
-    for i, nlines in enumerate(np.insert(np.diff(lines), 0, 1)):
-        for _ in range(nlines - 1):
+        locs = np.array([find(feature, value) for feature, value in zip(features, target)])
+        nf_products = np.append(nfeatures[1:], 1)
+        for i in range(len(nf_products)-2, -1, -1):
+            nf_products[i] *= nf_products[i+1]
+        C = (np.array(i) for i in itertools.product([0,1], repeat=len(nf_products)))
+        lines = np.array([np.dot(nf_products, locs + next(C)) for i in range(2**len(nf_products))])
+        performance_data = np.zeros((2**len(nf_products), 3))
+        for _ in range(lines[0]):
             f.readline()
-        performance_data[i, :] = np.array(f.readline().split()[5:]).astype('float')
+        for i, nlines in enumerate(np.insert(np.diff(lines), 0, 1)):
+            for _ in range(nlines - 1):
+                f.readline()
+            performance_data[i, :] = np.array(f.readline().split()[5:]).astype('float')
 
-weights = []
-for feature, loc, target_coord in zip(features, locs, target):
-    weights.append((feature[loc+1] - target_coord) / (feature[loc+1] - feature[loc]))
-for weight in weights:
-    lower_values, upper_values = np.split(performance_data, 2, axis=0)
-    performance_data = weight * lower_values + (1-weight) * upper_values
+    weights = []
+    for feature, loc, target_coord in zip(features, locs, target):
+        weights.append((feature[loc+1] - target_coord) / (feature[loc+1] - feature[loc]))
+    for weight in weights:
+        lower_values, upper_values = np.split(performance_data, 2, axis=0)
+        performance_data = weight * lower_values + (1-weight) * upper_values
+    return performance_data[0]
 
-print(performance_data)
+
+def F_L(point):
+    with open('permap.txt', 'r') as f:
+        features = []
+        nfeatures = []
+        for _ in range(5):
+            s = f.readline().replace(',', '').split(' ')
+            nfeatures.append(num(s[2]))
+            features.append([num(v) for v in s[4:]])
+        f.readline()
+        locs = np.array([find(feature, value) for feature, value in zip(features, target)])
+        norm_point = tuple((pt_coord - feature[loc]) / (feature[loc+1] - feature[loc])
+                           for feature, pt_coord, loc in zip(features, point, locs))
+        ord_point_idx = np.argsort(norm_point)  # find Fortran function to sort and get permutations
+        s = [np.zeros_like(norm_point, dtype=int)]
+        for i, idx in enumerate(ord_point_idx[::-1]):
+            s2 = s[i].copy()
+            s2[idx] = 1
+            s.append(s2)
+        vertex_locs = [s + locs for s in s]
+        nf_products = np.append(nfeatures[1:], 1)
+        for i in range(len(nf_products) - 2, -1, -1):
+            nf_products[i] *= nf_products[i + 1]
+        lines = [np.dot(vertex_loc, nf_products) for vertex_loc in vertex_locs]
+        performance_data = np.zeros((len(nf_products)+1, 3))
+        for _ in range(int(lines[0])):
+            f.readline()
+        for i, nlines in enumerate(np.insert(np.diff(lines), 0, 1)):
+            for _ in range(nlines - 1):
+                f.readline()
+            performance_data[i, :] = np.array(f.readline().split()[5:]).astype('float')
+        for vertex_loc, pdat in zip(vertex_locs, performance_data):
+            print([(i, feature[i]) for i, feature in zip(vertex_loc, features)], pdat)
+        FL = performance_data[-1].copy()
+        p_dat_old = performance_data[-1].copy()
+        for i, p_dat in enumerate(performance_data[:-1][::-1]):
+            print(norm_point[ord_point_idx[i]], p_dat)
+            FL +=  (1 - norm_point[ord_point_idx[i]]) * (p_dat - p_dat_old)
+            p_dat_old = p_dat
+        return performance_data
+
+
+
+print(F_M(target))
+print(F_L(target))
+
+
+
+# sorted_weights = np.argsort(weights)
+#
+# s0 = np.ones_like(weights)
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Validation: interpolate using scipy
-from scipy.interpolate import interpn
-import pandas as pd
-permap = pd.read_table('permap.txt', skiprows=5, index_col=('Tin', 'RHin', 'Tout', 'mfr', 'f'))
-Pel, Qcs, Qcl = permap['Pel'], permap['Qcs'], permap['Qcl']
-dims = [len(feature) for feature in Pel.index.levels]
-Pel_values = Pel.values.reshape(*dims)
-Qcs_values = Qcs.values.reshape(*dims)
-Qcl_values = Qcl.values.reshape(*dims)
-
-print(interpn(tuple(features), Pel_values, np.array(target))[0],
-      interpn(tuple(features), Qcs_values, np.array(target))[0],
-      interpn(tuple(features), Qcl_values, np.array(target))[0])
+# from scipy.interpolate import interpn
+# import pandas as pd
+# permap = pd.read_table('permap.txt', skiprows=5, index_col=('Tin', 'RHin', 'Tout', 'mfr', 'f'))
+# Pel, Qcs, Qcl = permap['Pel'], permap['Qcs'], permap['Qcl']
+# dims = [len(feature) for feature in Pel.index.levels]
+# Pel_values = Pel.values.reshape(*dims)
+# Qcs_values = Qcs.values.reshape(*dims)
+# Qcl_values = Qcl.values.reshape(*dims)
+#
+# print(interpn(tuple(features), Pel_values, np.array(target))[0],
+#       interpn(tuple(features), Qcs_values, np.array(target))[0],
+#       interpn(tuple(features), Qcl_values, np.array(target))[0])
